7858cbeb1a74af2f7c86be0e1bef87361aa3c278
Syed Shariq <25cs1940@amceducation.in>
2026-04-11 15:42:26 +0530

    Migrate secrets to envirnment variables and update docker config

--- priceTracker/src/main/java/com/priceTracker/Security/JwtUtil.java
+++ priceTracker/src/main/java/com/priceTracker/Security/JwtUtil.java
@@ -6,13 +6,15 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "mysecretkeymysecretkeymysecretkeymysecretkey";
+    @Value("${jwt.secret}")
+    private String SECRET;
 
     public String generateToken(UserDetails userDetails) {
 
